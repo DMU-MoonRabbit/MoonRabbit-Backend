ec604963b7266b9b03ee50a7391bcf3b1a0dd884
HYEON <74049556+dohy-eon@users.noreply.github.com>
2025-04-07 22:03:26 +0900

    #23 [fix] SecurityConfig와 SwaggerConfig의 충돌문제 수정

--- src/main/java/com/bigpicture/moonrabbit/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bigpicture/moonrabbit/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.bigpicture.moonrabbit.global.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -12,9 +13,11 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     // 커스텀 예외 처리
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handleCustomException(CustomException ex) {
+        ex.printStackTrace();
         ErrorCode errorCode = ex.getErrorCode();
         ErrorResponse response = new ErrorResponse(errorCode);
         return new ResponseEntity<>(response, errorCode.getStatus());
@@ -23,6 +26,7 @@ public class GlobalExceptionHandler {
     // 입력값 검증 예외 처리
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        ex.printStackTrace();
         BindingResult bindingResult = ex.getBindingResult();
         Map<String, String> errors = new HashMap<>();
 
@@ -35,7 +39,15 @@ public class GlobalExceptionHandler {
 
     // 기타 예외 처리
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex) {
+    public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex, HttpServletRequest request) {
+        ex.printStackTrace();
+        String uri = request.getRequestURI();
+
+        // Swagger 관련 요청은 예외 재던짐
+        if (uri.startsWith("/v3/api-docs") || uri.startsWith("/swagger-ui")) {
+            throw new RuntimeException(ex); // 또는 throw ex;
+        }
+
         ErrorResponse response = new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(response, ErrorCode.INTERNAL_SERVER_ERROR.getStatus());
     }
