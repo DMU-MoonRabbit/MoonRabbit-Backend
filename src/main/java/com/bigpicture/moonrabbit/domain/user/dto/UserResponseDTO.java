5022d8bf997cf8eda08d028dbd74b3f9205ff5a6
HYEON <74049556+dohy-eon@users.noreply.github.com>
2025-06-08 04:17:56 +0900

    #51 [refactor] 유저 조회 정보 수정

--- src/main/java/com/bigpicture/moonrabbit/domain/user/dto/UserResponseDTO.java
+++ src/main/java/com/bigpicture/moonrabbit/domain/user/dto/UserResponseDTO.java
@@ -8,11 +8,13 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class UserResponseDTO {
+    private Long id;
     private String email;
     private String nickname;
     private String password;
 
     public UserResponseDTO(User user) {
+        this.id = user.getId();
         this.email = user.getEmail();
         this.nickname = user.getNickname();
         this.password = user.getPassword();
