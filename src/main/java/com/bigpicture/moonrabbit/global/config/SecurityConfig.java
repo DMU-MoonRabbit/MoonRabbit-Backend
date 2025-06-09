90441c8bbbeceee0dda06b10445a4dbac6798020
HYEON <74049556+dohy-eon@users.noreply.github.com>
2025-06-09 11:43:17 +0900

    hotfix: CORS 권한 설정 수정

--- src/main/java/com/bigpicture/moonrabbit/global/config/SecurityConfig.java
+++ src/main/java/com/bigpicture/moonrabbit/global/config/SecurityConfig.java
@@ -73,7 +73,10 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:5173",
-                                         "https://moonrabbit-api-kro.kr"
+                                         "https://moonrabbit-api.kro.kr",
+                                         "https://moonrabbit-web.netlify.app",
+                                         "https://moonrabbit-web.kro.kr"
+                                         
         )); // 개발용 도메인
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
