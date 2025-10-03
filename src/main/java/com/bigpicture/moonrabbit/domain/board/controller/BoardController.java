c0d12a03502e25b1cfe61533ddb4e0b4818aee02
Dohyeon <do.hy824@gmail.com>
2025-10-04 03:51:45 +0900

    hotfix: 미임포트된 클래스 추가

--- src/main/java/com/bigpicture/moonrabbit/domain/board/controller/BoardController.java
+++ src/main/java/com/bigpicture/moonrabbit/domain/board/controller/BoardController.java
@@ -15,6 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+import java.util.HashMap;
 
 @RestController
 @RequiredArgsConstructor
--- src/main/java/com/bigpicture/moonrabbit/domain/board/repository/BoardRepository.java
+++ src/main/java/com/bigpicture/moonrabbit/domain/board/repository/BoardRepository.java
@@ -2,6 +2,8 @@ package com.bigpicture.moonrabbit.domain.board.repository;
 
 import com.bigpicture.moonrabbit.domain.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
