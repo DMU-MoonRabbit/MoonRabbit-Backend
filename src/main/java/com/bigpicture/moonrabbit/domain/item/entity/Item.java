68a1d05af5f7272864cf74ee1a0cfb1e4726eb87
Dohyeon Choi <do.hy824@gmail.com>
2025-10-18 19:08:22 +0900

    #99 [feat] 아이템 수정 삭제 기능 구현

--- src/main/java/com/bigpicture/moonrabbit/domain/item/entity/Item.java
+++ src/main/java/com/bigpicture/moonrabbit/domain/item/entity/Item.java
@@ -3,9 +3,11 @@ package com.bigpicture.moonrabbit.domain.item.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class Item {
     @Id
